db1d72c2fea263b93b223bfadc652da9796a9c5c
cinnamon0703 <147472099+cinnamon0703@users.noreply.github.com>
2023-10-29 19:48:28 +0900

    0.01로 바꾸자
    ? 이번엔 또 더 미끄러지네?

--- modelling a force/sketch.js
+++ modelling a force/sketch.js
@@ -24,7 +24,7 @@ function draw() {
 
   if (mover.contactEdge()) {
     //{!5 .bold}
-    let c = 10;
+    let c = 0.01;
     let friction = mover.velocity.copy();
     friction.mult(-1);
     friction.setMag(c);
