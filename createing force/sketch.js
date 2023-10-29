ed9e3bc34b408097eef41443fa2d976cfe55e222
cinnamon0703 <147472099+cinnamon0703@users.noreply.github.com>
2023-10-29 18:55:35 +0900

    createing force

---
+++ createing force/sketch.js
@@ -0,0 +1,40 @@
+let moverA;
+let moverB;
+let windForceApplied = false;
+
+function setup() {
+  createCanvas(640, 240);
+  moverA = new Mover(200, 30, 10);
+  moverB = new Mover(440, 30, 2);
+  createP('Click mouse to apply wind force.');
+}
+
+function draw() {
+  background(255);
+
+  let gravity = createVector(0, 0.1);
+
+  let gravityA = p5.Vector.mult(gravity, moverA.mass);
+  moverA.applyForce(gravityA);
+
+  let gravityB = p5.Vector.mult(gravity, moverB.mass);
+  moverB.applyForce(gravityB);
+
+  if (mouseIsPressed) {
+    let wind = createVector(0.1, 0);
+    moverA.applyForce(wind);
+    moverB.applyForce(wind);
+  } else {
+    let counter = createVector(-0.1, 0);
+    moverA.applyForce(counter);
+    moverB.applyForce(counter);
+  }
+
+  moverA.update();
+  moverA.display();
+  moverA.checkEdges();
+
+  moverB.update();
+  moverB.display();
+  moverB.checkEdges();
+}
