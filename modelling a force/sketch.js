6762ed9c20b97cd14cda5f4525ba6c2e24c7e67a
cinnamon0703 <147472099+cinnamon0703@users.noreply.github.com>
2023-10-29 19:49:43 +0900

    2로 바꿔보자
    달라지는게 없는 거 같은데... c가 아닌가?

--- modelling a force/sketch.js
+++ modelling a force/sketch.js
@@ -24,7 +24,7 @@ function draw() {
 
   if (mover.contactEdge()) {
     //{!5 .bold}
-    let c = 0.01;
+    let c = 2;
     let friction = mover.velocity.copy();
     friction.mult(-1);
     friction.setMag(c);
